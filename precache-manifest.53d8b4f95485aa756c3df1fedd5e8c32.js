6bd14118f072639b328fd4b9a2170bca9cd71691
Kent McKinney <kent.p.mckinney@gmail.com>
2020-05-11 09:36:54 -0700

    Updates

---
+++ precache-manifest.53d8b4f95485aa756c3df1fedd5e8c32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e343608c46ee442dd5bb258cf3079f5",
+    "url": "/epi-bird-sounds/index.html"
+  },
+  {
+    "revision": "b48ec7e4e14a7869084c",
+    "url": "/epi-bird-sounds/static/css/main.074f8bc8.chunk.css"
+  },
+  {
+    "revision": "690c08a36ec138e329ac",
+    "url": "/epi-bird-sounds/static/js/2.56eef318.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/epi-bird-sounds/static/js/2.56eef318.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b48ec7e4e14a7869084c",
+    "url": "/epi-bird-sounds/static/js/main.1848db90.chunk.js"
+  },
+  {
+    "revision": "0ba2f331dc0191c9e0b7",
+    "url": "/epi-bird-sounds/static/js/runtime-main.de2b1b5d.js"
+  }
+]);
